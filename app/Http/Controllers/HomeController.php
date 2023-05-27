f1f1b8264ac60b4eed4f0623fd99982e027fd4a3
master <tanvir.khan@penitt.com>
2023-05-28 02:10:22 +0600

    cc
    Signed-off-by: master <tanvir.khan@penitt.com>

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -51,26 +51,32 @@ class HomeController extends Controller{
     }
     //get my notification
     public function get_my_notification(){
-        $notify = Notification::where('is_view',0)->where('user_id',Auth::user()->id)->take(15)->get();
+        $notify = Notification::where('user_id',Auth::user()->id)->orderBy('id','desc')->take(15)->get();
         $select = '';
         if($notify){
             foreach($notify as $key=>$row){
-                $select .= '<div class="dropdown-item">';
-                    $select .= '<div class="media server-log">';
-                        $select .= '<img src="'.asset($row->creator_image).'" class="img-fluid me-2" alt="avatar">';
-                        $select .= '<div class="media-body">';
-                            $select .= '<div class="data-info">';
-                                $select .= '<h6 class="">'.$row->creator_name.'</h6>';
-                                $select .= '<p>'.$row->description.'</p>';
-                                $select .= '<p class="">'.Service::timeLeft($row->create_date).'</p>';
-                            $select .= '</div>';
-                            $select .= '<div class="icon-status">';
-                                $select .= '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>';
-                            $select .= '</div>';
+                if($row->is_view==0){
+                    $select .= '<div class="tr-bg dropdown-item">';
+                }else{
+                    $select .= '<div class="dropdown-item">';
+                }
+                $select .= '<div class="media server-log">';
+                    $select .= '<img src="'.asset($row->creator_image).'" class="img-fluid me-2" alt="avatar">';
+                    $select .= '<div class="media-body">';
+                        $select .= '<div class="data-info">';
+                            $select .= '<h6 class="">'.$row->creator_name.'</h6>';
+                            $select .= '<a href="'.url("task/details/".$row->slug).'"><p>'.$row->description.'</p></a>';
+                            $select .= '<p class="">'.Service::timeLeft($row->create_date).'</p>';
+                        $select .= '</div>';
+                        $select .= '<div class="icon-status">';
+                            $select .= '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>';
                         $select .= '</div>';
                     $select .= '</div>';
                 $select .= '</div>';
+            $select .= '</div>';
             }
+            //update notification first
+            $update = Notification::where('user_id',Auth::user()->id)->update(['is_view'=>1]);
             $data['result'] = array(
                 'key'=>200,
                 'val'=>$select
--- resources/views/adminpanel.blade.php
+++ resources/views/adminpanel.blade.php
@@ -81,7 +81,7 @@
 
                 <li class="nav-item dropdown notification-dropdown">
                     @php
-                        $notify_count = App\Models\Notification\Notification::where('user_id',Auth::user()->id)->count();
+                        $notify_count = App\Models\Notification\Notification::where('is_view',0)->where('user_id',Auth::user()->id)->count();
                     @endphp
                     <a onclick="show_notifications()" href="javascript:void(0);" class="nav-link dropdown-toggle" id="notificationDropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bell"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path><path d="M13.73 21a2 2 0 0 1-3.46 0"></path></svg>
@@ -331,6 +331,7 @@
                     if(data['result']['key']===200){
                         console.log(data['result']['val']);
                         $('#notification-item').html(data['result']['val']);
+                        $('#notification-badge').html(0);
                     }
                 });
             }
